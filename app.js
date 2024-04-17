1b1413e7df1f5c5c1c724915fe949ac00043de1f
Muhammad Momin <160387789+MuhammadMomin095@users.noreply.github.com>
2024-04-17 14:45:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var count = 5;
+if (count > 0) {
+    console.log("count is greater then o.");
+}
+function add(a, b) {
+    return a + b;
+}
---
+++ app.ts
@@ -0,0 +1,10 @@
+let count = 5;
+
+
+if (count > 0) {
+    console.log("count is greater then o.");   
+}
+
+function add(a, b) {
+    return a + b;
+}
